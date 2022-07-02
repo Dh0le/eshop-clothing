7dd8505b269956432477334af68af7f514117b17
Dh0le <i@zjiaqi.com>
2022-07-02 17:54:22 -0500

    refactor: refactor file

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -1,5 +1,5 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
-import { USER_ACTION_TYPES } from "./user-reducer";
+import { USER_ACTION_TYPES } from "./user.type";
 export const setCurrentUser = (user) => {
   return createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 };
